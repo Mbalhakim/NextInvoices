ba7bd0c4d29ee1a314436f045aa3ad383c25b5f6
Mbalhakim <mb.alhakim@gmail.com>
2024-04-23 06:21:27 +0200

    added search queries

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,3 +1,4 @@
+import { fetchCardData } from '@/app/lib/data';
 import {
   BanknotesIcon,
   ClockIcon,
@@ -11,6 +12,29 @@ const iconMap = {
   pending: ClockIcon,
   invoices: InboxIcon,
 };
+
+
+export default async function CardWrapper() {
+  const {
+    numberOfInvoices,
+    numberOfCustomers,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+ 
+  return (
+    <>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
 
 
 export function Card({
@@ -47,3 +71,4 @@ export function Card({
     </div>
   );
 }
+
